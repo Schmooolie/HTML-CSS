8823b3512a4783c3215376b74a7175d55d413c53
Schmooolie <m-ruckpaul@t-online.de>
2025-06-10 10:46:48 +0200

    Initial commit

---
+++ js/test.js
@@ -0,0 +1,4 @@
+function clicked() {
+    document.getElementById('test-inner-button').innerHTML = 'Oha das tat tut weh!';
+}
+// Ist erstmal egal aber vielleicht auch selbst erklärend, aber so haste mal etwas kleines gesehen.
